6f101628718c41799a5e40d9df16ac63dae8067d
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2021-12-27 12:00:44 +0500

    BE-Category start

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -74,9 +74,11 @@ const userCtrl = {
             return res.status(500).json({msg: error.message});
         }
     },
-    getUser:(req, res) => {
+    getUser: async (req, res) => {
         try {
-            res.json(req.user)
+            const user = await Users.findById(req.user.id).select("-password")
+            if(!user) return res.status(400).json({msg: "User does not exist"})
+            res.json(user)
         } catch (error) {
             return res.status(500).json({msg: error.message});
         }
