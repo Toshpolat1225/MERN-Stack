7c7b7854abb0542bd788d3aaf177fdbdd473241a
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-03 15:22:03 +0500

    1.36.18

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -11,7 +11,14 @@ const categoryCtrl = {
     },
     createCategory: async(req, res)=>{
         try {
-            res.json("Category admindan qoshish")
+            // if user have role = 1 ---> admin
+            // only admin can create, delete and update category
+            const {name} = req.body;
+            const category = await Category.findOne({name})
+            if(category) return res.status(400).json({msg: "This category already exists"})
+            const newCategory = new Category({name})
+            await newCategory.save()
+            res.json("Category admindan qoshildi")
         } catch (error) {
             return res.status(500).json({msg: error.message})
         }
