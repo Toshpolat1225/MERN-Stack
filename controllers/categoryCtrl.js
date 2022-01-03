d6ce005eceb8bed1f5e1321209ea1a60a9f2aea8
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-03 11:44:31 +0500

    adminAuth role = 1

--- controllers/categoryCtrl.js
+++ controllers/categoryCtrl.js
@@ -8,6 +8,13 @@ const categoryCtrl = {
         } catch (error) {
             return res.status(500).json({msg: error.message})
         }
+    },
+    createCategory: async(req, res)=>{
+        try {
+            res.json("Category admindan qoshish")
+        } catch (error) {
+            return res.status(500).json({msg: error.message})
+        }
     }
 }
 module.exports = categoryCtrl
