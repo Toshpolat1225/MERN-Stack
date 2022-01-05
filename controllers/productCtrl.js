78fab1f1936a440a0c646e96a08c345c83caa6bd
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-05 11:49:02 +0500

    Update and Delete Product

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -23,40 +23,34 @@ const productCtrl = {
         try {
             const {product_id, title, price, description, content, images, category} = req.body;
             if(!images) return res.status(400).json({msg: "Rasm jo'natilmadi"})
-
             const product = await Products.findOne({product_id})
             if(product)
                 return res.status(400).json({msg: "Bu mahsulot o'zi bor"})
-
             const newProduct = new Products({
                  product_id, title: title.toLowerCase(), price, description, content, images, category
             })
-
             await newProduct.save()
             res.json({msg: "Product yaratildi"})
-
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
     deleteProduct: async(req, res) =>{
         try {
-            // await Products.findByIdAndDelete(req.params.id)
-            // res.json({msg: "Deleted a Product"})
+            await Products.findByIdAndDelete(req.params.id)
+            res.json({msg: "Product o'chdi"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
     },
     updateProduct: async(req, res) =>{
         try {
-            // const {title, price, description, content, images, category} = req.body;
-            // if(!images) return res.status(400).json({msg: "No image upload"})
-
-            // await Products.findOneAndUpdate({_id: req.params.id}, {
-            //     title: title.toLowerCase(), price, description, content, images, category
-            // })
-
-            // res.json({msg: "Updated a Product"})
+            const {title, price, description, content, images, category} = req.body;
+            if(!images) return res.status(400).json({msg: "Rasm jo'natilmadi"})
+            await Products.findOneAndUpdate({_id: req.params.id}, {
+                 title: title.toLowerCase(), price, description, content, images, category
+            })
+            res.json({msg: "Product yangilandi"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
