04fe4135cc76d60e2f7ca21137d7eae0328f7a94
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-11 14:13:40 +0500

    Client - Products start

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -10,12 +10,12 @@ import NotFound from './utils/NotFound/NotFound';
 const Pages = () => {
     return (
         <Routes>
-        <Route path="/" exact components={Products}/>
-        <Route path="/login" exact components={Login}/>
-        <Route path="/register" exact components={Register}/>
-        <Route path="/cart" exact components={Cart}/>
+        <Route path="/" exact element={<Products/>}/>
+        <Route path="/login" exact element={<Login/>}/>
+        <Route path="/register" exact element={<Register/>}/>
+        <Route path="/cart" exact element={<Cart/>}/>
 
-        <Route path="*" exact components={NotFound}/>
+        <Route path="*" exact element={<NotFound/>}/>
         </Routes>
     )
 }
