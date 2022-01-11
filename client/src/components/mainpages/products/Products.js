d3d0f89fc7690793248755a8ee948f410e95e334
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-11 15:24:31 +0500

    3:24:18

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalState } from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
 
 const Products = () => {
+    const state =useContext(GlobalState)
+    const [products] = state.productsAPI.products
     return (
-        <div>
-            Products list
+        <div className="products">
+            {
+                products.map(product =>{
+                    return <ProductItem />
+                })
+            }
         </div>
     )
 }
