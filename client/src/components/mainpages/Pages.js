a67af58b66f951e255e389791d5919bd7f5274aa
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-11 12:36:13 +0500

    Switch to Routes

--- client/src/components/mainpages/Pages.js
+++ client/src/components/mainpages/Pages.js
@@ -1,21 +1,19 @@
 import React from 'react'
-import {Switch, Route} from "react-router-dom"
-import Products from './products/Products'
-import Login from './auth/Login'
-import Register from './auth/Register'
-import Cart from './cart/Cart'
+import {Routes, Route} from "react-router-dom";
+import Products from './products/Products';
+import Login from './auth/Login';
+import Register from './auth/Register';
+import Cart from './cart/Cart';
 
 
 const Pages = () => {
     return (
-        <Switch>
-
+        <Routes>
         <Route path="/" exact components={Products}/>
         <Route path="/login" exact components={Login}/>
         <Route path="/register" exact components={Register}/>
         <Route path="/cart" exact components={Cart}/>
-
-        </Switch>
+        </Routes>
     )
 }
 
