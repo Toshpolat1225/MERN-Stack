69b97ae23a377b15fd43b8162e81a0a9e76ad80f
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-16 16:49:14 +0500

    recomendenadsiya

--- client/src/components/mainpages/detailProduct/DetailProduct.js
+++ client/src/components/mainpages/detailProduct/DetailProduct.js
@@ -1,6 +1,7 @@
 import React, {useContext, useState, useEffect} from 'react'
 import {useParams, Link} from 'react-router-dom'
 import {GlobalState} from '../../../GlobalState'
+import ProductItem from '../utils/productItem/ProductItem'
 
 function  DetailProduct () {
     const params = useParams()
@@ -8,15 +9,16 @@ function  DetailProduct () {
     const [products] = state.productsAPI.products
     const [detailProduct, setDetailProduct]= useState([])
     useEffect(() =>{
-        if(params){
+        if(params.id){
             products.forEach(product =>{
                 if(product._id === params.id) setDetailProduct(product)
             })
         }
-    },[params,products])
+    },[params.id,products])
     console.log(detailProduct)
     if(detailProduct.length === 0) return null;
     return (
+        <>
         <div className="detail">
            <img src={detailProduct.images.url} alt="" />
            <div className="box-detail">
@@ -33,6 +35,21 @@ function  DetailProduct () {
                     </Link>
                 </div>
         </div>
+        
+        <div>
+            <h2>Realted products</h2>
+            <div className="products">
+                {
+                    products.map(product =>{
+                        return product.category === detailProduct.category
+                            ? < ProductItem key={product._id} product={product}/> : null
+                    })
+                }
+            </div>
+
+        </div>
+        
+        </>
     )
 }
 
