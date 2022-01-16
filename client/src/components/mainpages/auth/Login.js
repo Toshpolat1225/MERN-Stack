95ca662b8fa8ceaa0426151b40f08a891fcd2475
Toshpolat1225 <turaqulovtoshpulat@mail.ru>
2022-01-16 17:12:14 +0500

    4.00.00

--- client/src/components/mainpages/auth/Login.js
+++ client/src/components/mainpages/auth/Login.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Login = () => {
+    const [user, setUser] = useState({
+        email: '', password: ''
+    })
     return (
         <div>
             Login Component
